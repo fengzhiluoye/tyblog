deaca3d768fb39652102426a4c3d688db7d8d456
fengzhiluoye <2535606879@qq.com>
2013-11-29 18:17:03 +0800

    my commit

---
+++ test1.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312">
+<title>PHP标记示例</title>
+</head>
+<body>
+<?echo "<h3>使用简短风格</h3>";?>
+<?php echo "<h3>使用XML风格</h3>";?>
+<script language="php">echo "<h3>这是脚本风格</h3>";</script>
+<%echo "<h3>这是ASP风格</h3>";%>
+</body>
+</html>
